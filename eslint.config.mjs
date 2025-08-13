1b26ee9ec9a7e311a90547420f3df439d03baca2
Carlos Ponton <carlitosponton@hotmail.com>
2025-08-13 20:36:08 +0200

    Install and configure eslint. Fix some lint issues

---
+++ eslint.config.mjs
@@ -0,0 +1,75 @@
+import eslint from '@eslint/js'
+import stylistic from '@stylistic/eslint-plugin'
+import eslintPluginAstro from 'eslint-plugin-astro'
+import importPlugin from 'eslint-plugin-import'
+import unusedImports from 'eslint-plugin-unused-imports'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  {
+    ignores: [
+      '**/public',
+      '**/dist',
+      '**/dist/*',
+      '**/tests/*',
+      'coverage',
+      '.astro/*',
+      'node_modules/*',
+    ],
+  },
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  eslintPluginAstro.configs.recommended,
+  {
+    files: [ '**/*.{tsx,jsx,mjs,cjs,js,ts,astro}' ],
+    languageOptions: { ecmaVersion: 'latest', globals: globals.browser },
+    plugins: {
+      import: importPlugin,
+      'unused-imports': unusedImports,
+      '@stylistic': stylistic,
+    },
+    rules: {
+      // @stylistic
+      '@stylistic/array-bracket-spacing': [ 'error', 'always' ],
+      '@stylistic/object-curly-spacing': [ 'error', 'always' ],
+      '@stylistic/type-annotation-spacing': 'error',
+      '@stylistic/quotes': [ 'error', 'single' ],
+      '@stylistic/semi': [ 'error', 'never' ],
+      '@stylistic/indent': [ 'error', 2 ],
+      '@stylistic/max-len': [ 'error', { code: 136 } ],
+      '@stylistic/key-spacing': [ 'error', { beforeColon: false, afterColon: true } ],
+
+
+      // imports
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+      'import/order': [
+        'warn',
+        {
+          alphabetize: { caseInsensitive: true, order: 'asc' },
+          groups: [ 'builtin', 'external', 'sibling', 'parent' ],
+          'newlines-between': 'always',
+          pathGroupsExcludedImportTypes: [ 'builtin' ],
+          pathGroups: [
+            {
+              group: 'external',
+              pattern: '@/**',
+              position: 'after',
+            },
+          ],
+        },
+      ],
+    },
+  },
+)
