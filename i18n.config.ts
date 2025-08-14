15183b495efaba17c8cfce65390a32d0d375cabf
Carlos Ponton <carlitosponton@hotmail.com>
2025-08-14 21:41:01 +0200

    Add a first function version with translations

---
+++ i18n.config.ts
@@ -0,0 +1,8 @@
+const supportedLanguages = [ 'en', 'es' ] as const
+
+export type Locale = typeof supportedLanguages[number]
+
+export const i18nConfig = {
+  locales: supportedLanguages,
+  defaultLocale: 'en',
+}
